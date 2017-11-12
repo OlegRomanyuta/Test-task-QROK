0eeeaa60604fcf0847282ba8186ac819698b4699
Admin <mastnk@mail.ru>
2017-11-12 16:12:28 +0200

    disabled authentication

--- spring-boot-config/src/main/java/ua/oleg/romanyuta/spring/config/Main.java
+++ spring-boot-config/src/main/java/ua/oleg/romanyuta/spring/config/Main.java
@@ -7,15 +7,15 @@ import org.springframework.boot.autoconfigure.domain.EntityScan;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
-import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
-import org.springframework.security.config.annotation.authentication.configurers.GlobalAuthenticationConfigurerAdapter;
-import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
-import org.springframework.security.core.authority.AuthorityUtils;
-import org.springframework.security.core.userdetails.User;
-import org.springframework.security.core.userdetails.UserDetailsService;
-import org.springframework.security.core.userdetails.UsernameNotFoundException;
+//import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+//import org.springframework.security.config.annotation.authentication.configurers.GlobalAuthenticationConfigurerAdapter;
+//import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+//import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+//import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+//import org.springframework.security.core.authority.AuthorityUtils;
+//import org.springframework.security.core.userdetails.User;
+//import org.springframework.security.core.userdetails.UserDetailsService;
+//import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import ua.oleg.romanyuta.dao.AccountRepository;
 import ua.oleg.romanyuta.domain.Account;
 
@@ -31,41 +31,41 @@ public class Main {
     }
 
 
-    @EnableWebSecurity
-    @Configuration
-    public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
-
-        @Override
-        protected void configure(HttpSecurity http) throws Exception {
-            http.authorizeRequests().anyRequest().fullyAuthenticated().and().
-                    httpBasic().and().
-                    csrf().disable();
-        }
-    }
-
-    @Configuration
-    public class WebSecurityConfiguration extends GlobalAuthenticationConfigurerAdapter {
-
-        @Autowired
-        AccountRepository accountRepository;
-
-        @Override
-        public void init(AuthenticationManagerBuilder auth) throws Exception {
-            auth.userDetailsService(userDetailsService());
-        }
-
-        @Bean
-        UserDetailsService userDetailsService() {
-            return username -> {
-                Account account = accountRepository.findByUsername(username);
-                if(account != null) {
-                    return new User(account.getUsername(), account.getPassword(), true, true, true, true,
-                            AuthorityUtils.createAuthorityList("USER"));
-                } else {
-                    throw new UsernameNotFoundException("could not find the user '"
-                            + username + "'");
-                }
-            };
-        }
-    }
+//    @EnableWebSecurity
+//    @Configuration
+//    public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
+//
+//        @Override
+//        protected void configure(HttpSecurity http) throws Exception {
+//            http.authorizeRequests().anyRequest().fullyAuthenticated().and().
+//                    httpBasic().and().
+//                    csrf().disable();
+//        }
+//    }
+//
+//    @Configuration
+//    public class WebSecurityConfiguration extends GlobalAuthenticationConfigurerAdapter {
+//
+//        @Autowired
+//        AccountRepository accountRepository;
+//
+//        @Override
+//        public void init(AuthenticationManagerBuilder auth) throws Exception {
+//            auth.userDetailsService(userDetailsService());
+//        }
+//
+//        @Bean
+//        UserDetailsService userDetailsService() {
+//            return username -> {
+//                Account account = accountRepository.findByUsername(username);
+//                if(account != null) {
+//                    return new User(account.getUsername(), account.getPassword(), true, true, true, true,
+//                            AuthorityUtils.createAuthorityList("USER"));
+//                } else {
+//                    throw new UsernameNotFoundException("could not find the user '"
+//                            + username + "'");
+//                }
+//            };
+//        }
+//    }
 }
